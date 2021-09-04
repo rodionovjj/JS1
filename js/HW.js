41a522d9a47815fda4d8649940a0b71c0d7ef65d
Dmitriy Rodionov <rodionov-dm@bk.ru>
2021-09-04 10:21:49 +0300

    FizzBizz homework

--- js/HW.js
+++ js/HW.js
@@ -1,1 +1,17 @@
-  
+const fizzBuzz = (begin, end) => {              
+    for (let i = begin; (i-1) < end; i++){       
+        if (i % 3 === 0 && i % 5 === 0) {       
+            console.log ("FizzBuzz");           
+        }
+        if (i % 3 === 0){
+            console.log ("Fizz");
+        }
+        if (i % 5 === 0){
+            console.log ("Buzz");
+        }
+        else{
+            console.log (i);
+        }
+    }
+}
+console.log (fizzBuzz (1, 25))
